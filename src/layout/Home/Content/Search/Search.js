19c7671b9bc026a7e6a41cb0e70630eb982beca1
vuduchiieu <vuduchiieu@gmail.com>
2024-02-13 23:43:19 +0700

    fix search

--- src/layout/Home/Content/Search/Search.js
+++ src/layout/Home/Content/Search/Search.js
@@ -3,11 +3,12 @@ import styles from "./search.module.scss";
 import React, { useEffect, useState } from "react";
 import { allSong } from "~/db/songs";
 import { useAppContext } from "~/component/context/AppContext";
+import icon from "~/assets/icon";
 
 const cx = classNames.bind(styles);
 
 function Search({ searchValue }) {
-  const { handleSongs, play } = useAppContext();
+  const { handleSongs, play, handleLikeToggle, like } = useAppContext();
   const [result, setResult] = useState([]);
   useEffect(() => {
     if (!searchValue) {
@@ -24,13 +25,21 @@ function Search({ searchValue }) {
       {result.map((item, i) => (
         <div
           key={i}
-          onClick={() => handleSongs(item)}
           className={cx("song", { active: play.title === item.title })}
         >
-          <img src={item.img} alt="" />
-          <h3>{item.title}</h3>
+          <div className={cx("title")} onClick={() => handleSongs(item)}>
+            <img src={item.img} alt="" />
+            <h3>{item.title}</h3>
+          </div>
           <div className={cx("info")}>
             <p>{item.name}</p>
+            <button onClick={() => handleLikeToggle(item.title)}>
+              {like[item.title] ? (
+                <img src={icon.heartActive} style={{ filter: "none" }} alt="" />
+              ) : (
+                <img src={icon.heart} alt="" />
+              )}
+            </button>
           </div>
         </div>
       ))}
