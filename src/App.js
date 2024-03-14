f9cd90c5b9a5d3dc55779a84e24704c163f16d10
vuduchiieu <vuduchiieu@gmail.com>
2024-03-14 19:37:47 +0700

    fix react title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React, { useEffect } from "react";
 import classNames from "classnames/bind";
 import Home from "~/layout/Home/Home.js";
 import { useAppContext } from "./component/context/AppContext";
@@ -8,7 +7,12 @@ const cx = classNames.bind();
 
 function App() {
   const { themeMode, play } = useAppContext();
-
+  useEffect(() => {
+    document.title =
+      play.title && play.author
+        ? `${play.title} - ${play.author}`
+        : "Stave - Web để nghe nhạc";
+  }, [play]);
   return (
     <div
       className={cx("App", {
@@ -16,13 +20,6 @@ function App() {
       })}
     >
       <Home />
-      <Helmet>
-        <title>
-          {play.title && play.author
-            ? `${play.title} - ${play.author}`
-            : "Stave - Web để nghe nhạc"}
-        </title>
-      </Helmet>
     </div>
   );
 }
