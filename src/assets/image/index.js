091f3be8a0fc69d882bfe2353cb870d7003129d3
vuduchiieu <vuduchiieu@gmail.com>
2024-03-29 19:19:18 +0700

    fix

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,7 +1,7 @@
 const image = {
   min: require("./min.jpg"),
   đen: require("./đen.jpeg"),
-  bray: require("./bray.webp"),
+  bray: require("./bray.jpg"),
   lowg: require("./lowg.jpeg"),
   isaac: require("./isaac.jpeg"),
   hienho: require("./hienho.jpeg"),
